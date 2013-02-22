9c7ddb9d503bb32e1110d6d977c2e3043dcdc870
Kim Altintop <kim.altintop@gmail.com>
2013-02-22 10:05:04 +0100

    Licensing ceremony

--- src/Network/Bouquet.hs
+++ src/Network/Bouquet.hs
@@ -4,6 +4,18 @@
 
 {-# OPTIONS_GHC -fno-warn-orphans       #-}
 
+-- |
+-- Module      : Network.Bouquet
+-- Copyright   : (c) 2013 Kim Altintop <kim.altintop@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Kim Altintop <kim.altintop@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
 module Network.Bouquet
     ( Bouquet
     , BouquetConf
--- src/Network/Bouquet/Internal.hs
+++ src/Network/Bouquet/Internal.hs
@@ -1,5 +1,17 @@
 {-# LANGUAGE BangPatterns #-}
 
+-- |
+-- Module      : Network.Bouquet.Internal
+-- Copyright   : (c) 2013 Kim Altintop <kim.altintop@gmail.com>
+-- License     : This Source Code Form is subject to the terms of
+--               the Mozilla Public License, v. 2.0.
+--               A copy of the MPL can be found in the LICENSE file or
+--               you can obtain it at http://mozilla.org/MPL/2.0/.
+-- Maintainer  : Kim Altintop <kim.altintop@gmail.com>
+-- Stability   : experimental
+-- Portability : non-portable (GHC extensions)
+--
+
 module Network.Bouquet.Internal
     ( measure
     ) where
