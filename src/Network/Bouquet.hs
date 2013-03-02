5f8b008f0e5451036fdd37806108499e22ee0e16
Kim Altintop <kim.altintop@gmail.com>
2013-03-02 02:32:24 +0100

    Fix exit condition

--- src/Network/Bouquet.hs
+++ src/Network/Bouquet.hs
@@ -177,12 +177,12 @@ retry b max_attempts = Bouquet $ ask >>= liftIO . go 0
     go attempt e = do
           _ <- liftIO $ threadDelay (backoff attempt * 1000)
           x <- (Right <$> runReaderT (unBouquet b) e) `catch` (return . Left)
-          if (isLeft x && attempt == max_attempts)
+          if (isRight x || attempt == max_attempts)
               then return x
               else go (attempt + 1) e
 
-    isLeft (Left _)  = True
-    isLeft (Right _) = False
+    isRight (Right _)= True
+    isRight (Left _)  = False
 
     backoff attempt = 1 `shiftL` (attempt - 1)
 
